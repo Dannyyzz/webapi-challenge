25cec9495cbf7b5674da7ae1c6762c79de473175
Daniel Viera <dannyzz@live.com>
2019-08-18 20:58:51 -0400

    Added json parsing && express

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const projects = require("./routes/projects");
-const projects = require("./routes/actions");
+const actions = require("./routes/actions");
 
 const server = express();
+
+server.use(express.json());
 
 server.use("/api/projects", projects);
 server.use("/api/actions", actions);

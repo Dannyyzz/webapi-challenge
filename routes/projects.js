9fae1f34171df3d4848a88ff76346fcbba10d045
Daniel Viera <dannyzz@live.com>
2019-08-18 21:12:01 -0400

    Added GET for projects

---
+++ routes/projects.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const Project = require("../data/helpers/projectModel");
+
+router.get("/", async (_req, res) => {
+  try {
+    const projects = await Project.get();
+
+    res.json({
+      projects
+    });
+  } catch (error) {
+    res.status(500).json({
+      errorMessage: "Internal Server Error",
+      message: error.message
+    });
+  }
+});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.detele("/:id", (req, res) => {});
+
+module.exports = router;
